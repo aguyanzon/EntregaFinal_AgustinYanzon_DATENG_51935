5304e1ef7600e41ce657448359295476595d60c2
Agustin Yanzon <aguyanzon@hotmail.com>
2023-07-26 19:23:31 -0300

    add all files

---
+++ dags/etl_finance.py
@@ -0,0 +1,94 @@
+# Este es el DAG que orquesta el ETL de la tabla users
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+
+from airflow.models import Variable
+
+from datetime import datetime, timedelta
+from os import environ as env
+
+
+QUERY_CREATE_TABLE = '''
+CREATE TABLE IF NOT EXISTS finance_spark (
+    "date_from" VARCHAR(30),
+    "open" VARCHAR(30),
+    "high" VARCHAR(30),
+    "low" VARCHAR(30),
+    "close" VARCHAR(30), 
+    "volume" VARCHAR(30),
+    "monthly variation" VARCHAR(30),
+    process_date VARCHAR(10),
+    symbol VARCHAR(30) distkey
+) sortkey(date_from);
+'''
+
+QUERY_CLEAN_PROCESS_DATE = """
+DELETE FROM finance_spark WHERE process_date = '{{ ti.xcom_pull(key="process_date") }}';
+"""
+
+# create function to get process_date and push it to xcom
+def get_process_date(**kwargs):
+    # If process_date is provided take it, otherwise take today
+    if (
+        "process_date" in kwargs["dag_run"].conf
+        and kwargs["dag_run"].conf["process_date"] is not None
+    ):
+        process_date = kwargs["dag_run"].conf["process_date"]
+    else:
+        process_date = kwargs["dag_run"].conf.get(
+            "process_date", datetime.now().strftime("%Y-%m-%d")
+        )
+    kwargs["ti"].xcom_push(key="process_date", value=process_date)
+
+
+defaul_args = {
+    "owner": "Agustin Yanzón",
+    "start_date": datetime(2023, 7, 11),
+    "retries": 0,
+    "retry_delay": timedelta(seconds=5),
+    "catchup": False,
+}
+
+with DAG(
+    dag_id="etl_finance",
+    default_args=defaul_args,
+    description="ETL de la tabla finance",
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
+    get_process_date_task = PythonOperator(
+        task_id="get_process_date",
+        python_callable=get_process_date,
+        provide_context=True,
+        dag=dag,
+    )
+
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        conn_id="redshift_default",
+        sql=QUERY_CREATE_TABLE,
+        dag=dag,
+    )
+
+    clean_process_date = SQLExecuteQueryOperator(
+        task_id="clean_process_date",
+        conn_id="redshift_default",
+        sql=QUERY_CLEAN_PROCESS_DATE,
+        dag=dag,
+    )
+
+    spark_etl_finance = SparkSubmitOperator(
+        task_id="spark_etl_finance",
+        application=f'{Variable.get("spark_scripts_dir")}/ETL_Finance.py',
+        conn_id="spark_default",
+        dag=dag,
+        driver_class_path=Variable.get("driver_class_path"),
+    )
+
+    get_process_date_task >> create_table >> clean_process_date >> spark_etl_finance
---
+++ scripts/ETL_Finance.py
@@ -0,0 +1,104 @@
+# Este script está pensado para correr en Spark y hacer el proceso de ETL de la tabla users
+
+import requests
+from datetime import datetime, timedelta
+from os import environ as env
+
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.window import Window
+from pyspark.sql.functions import concat, col, lit, when, expr, to_date, max, lag
+
+from commons import ETL_Spark
+
+class ETL_Finance(ETL_Spark):
+    def __init__(self, job_name=None):
+        super().__init__(job_name)
+        self.process_date = datetime.now().strftime("%Y-%m-%d")
+
+    def run(self):
+        process_date = datetime.now().strftime("%Y-%m-%d")
+        self.execute(process_date)
+
+    def extract(self, symbol):
+        """
+        Extrae datos de la API
+        """
+        print(">>> [E] Extrayendo datos de la API...")
+
+        try:
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey={env["API_KEY"]}'
+            response = requests.get(url)
+            json_data = response.json()["Monthly Time Series"]
+
+            df = (
+                self.spark.sparkContext.parallelize(list(json_data.items()))
+                .map(lambda x: Row(date=x[0], **x[1]))
+                .toDF(["date_from", "open", "high", "low", "close", "volume"])
+            )
+
+            df = df.withColumn("symbol", lit(symbol))
+
+            return df
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error de solicitud: {e}")
+            return None
+    
+    
+    def union_data(self):
+        print(">>> [E] Concatenando DataFrames...")
+
+        data_ibm = self.extract('IBM')
+        data_aapl = self.extract('AAPL')
+        data_tsla = self.extract('TSLA')
+        data = data_ibm.union(data_aapl).union(data_tsla)
+
+        return data
+
+
+    def transform(self, df_original):
+        """
+        Transforma los datos
+        """
+        print(">>> [T] Transformando datos...")
+
+        total_rows = df_original.count()
+        distinct_rows = df_original.dropDuplicates().count()
+
+        # Compara la cantidad de filas antes y después de eliminar los duplicados
+        if total_rows == distinct_rows:
+            print("El DataFrame no tiene duplicados.")
+        else:
+            print("El DataFrame tiene duplicados.")
+
+        window_spec = Window.partitionBy('symbol').orderBy('date_from')
+        df_original = df_original.withColumn('monthly variation', (col('close') - lag('close').over(window_spec)) / lag('close').over(window_spec) * 100)
+
+        return df_original
+
+
+    def load(self, df_final):
+        """
+        Carga los datos transformados en Redshift
+        """
+        print(">>> [L] Cargando datos en Redshift...")
+
+        df_final = df_final.withColumn("process_date", lit(self.process_date))
+        
+        df_final.write \
+            .format("jdbc") \
+            .option("url", env['REDSHIFT_URL']) \
+            .option("dbtable", f"{env['REDSHIFT_SCHEMA']}.finance_spark") \
+            .option("user", env['REDSHIFT_USER']) \
+            .option("password", env['REDSHIFT_PASSWORD']) \
+            .option("driver", "org.postgresql.Driver") \
+            .mode("append") \
+            .save()
+        
+        print(">>> [L] Datos cargados exitosamente")
+
+
+if __name__ == "__main__":
+    print("Corriendo script")
+    etl = ETL_Finance()
+    etl.run()
